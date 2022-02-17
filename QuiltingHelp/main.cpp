be47896635e9d4f9d370fad080d5cbff406cb24b
Sergey Zhuravlev <Sergey4Zhuravlev@gmail.com>
2022-02-17 21:32:47 +0300

    experiment

--- QuiltingHelp/main.cpp
+++ QuiltingHelp/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
     const auto startedAt = std::filesystem::canonical(firstArg).parent_path();
     const std::string imagePath = (startedAt / std::filesystem::path("nya.jpg")).string();
     Mat image;
-    image = imread(imagePath, 1);
+    image = imread(imagePath, IMREAD_COLOR);
     if (!image.data)
     {
         printf("No image data \n");
@@ -24,6 +24,23 @@ int main(int argc, char* argv[])
     }
     namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", image);
+    waitKey(0);
+    Mat imageSegment;
+    int spatialRadius = 35;
+    int colorRadius = 60;
+    int pyramidLevels = 3;
+    pyrMeanShiftFiltering(image, imageSegment, spatialRadius, colorRadius, pyramidLevels);
+    imshow("Display Image", imageSegment);
+    waitKey(0);
+    Mat imgray;
+    cvtColor(imageSegment, imgray, COLOR_BGR2GRAY);
+    imshow("Display Image", imgray);
+    waitKey(0);
+    std::vector< std::vector<Point> > contours;
+    findContours(imgray, contours, RETR_TREE, CHAIN_APPROX_NONE);
+    Mat mask = Mat::zeros(image.rows, image.cols, CV_8UC1);
+    drawContours(mask, contours, -1, Scalar(255), LINE_AA);
+    imshow("Display Image", mask);
     waitKey(0);
 
     return 0;
