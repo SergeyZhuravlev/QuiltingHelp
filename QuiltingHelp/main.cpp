7cdecf04969fc5536276a955591df4f0d08a83dd
Sergey Zhuravlev <Sergey4Zhuravlev@gmail.com>
2022-02-19 23:15:00 +0300

    experiment

--- QuiltingHelp/main.cpp
+++ QuiltingHelp/main.cpp
@@ -5,6 +5,23 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
+int edgeThresh = 1;
+
+Mat imageSegment;
+Mat image;
+
+void onTrackbar(int, void*)
+{
+    Mat imageSegmentEdge;
+    Canny(imageSegment, imageSegmentEdge, edgeThresh, edgeThresh*3);
+    imshow("Display Image", imageSegmentEdge);
+    /*std::vector< std::vector<Point> > contours;
+    findContours(imageSegmentEdge, contours, RETR_TREE, CHAIN_APPROX_NONE);
+    Mat mask = Mat::zeros(imageSegment.rows, imageSegment.cols, CV_8UC1);
+    drawContours(mask, contours, -1, Scalar(255), LINE_AA);
+    imshow("Display Image", mask);*/
+}
+
 #ifdef WIN32
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPreInst, LPSTR lpCmdLine, int nCmdShow)
 #else
@@ -14,8 +31,7 @@ int main(int argc, char* argv[])
     const auto allArgs = std::string_view(GetCommandLineA());
     const auto firstArg = allArgs.substr(1, allArgs.size()-3);
     const auto startedAt = std::filesystem::canonical(firstArg).parent_path();
-    const std::string imagePath = (startedAt / std::filesystem::path("nya.jpg")).string();
-    Mat image;
+    const std::string imagePath = (startedAt / std::filesystem::path("nya2.jpg")).string();
     image = imread(imagePath, IMREAD_COLOR);
     if (!image.data)
     {
@@ -25,23 +41,14 @@ int main(int argc, char* argv[])
     namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", image);
     waitKey(0);
-    Mat imageSegment;
     int spatialRadius = 35;
     int colorRadius = 60;
     int pyramidLevels = 3;
     pyrMeanShiftFiltering(image, imageSegment, spatialRadius, colorRadius, pyramidLevels);
     imshow("Display Image", imageSegment);
     waitKey(0);
-    Mat imgray;
-    cvtColor(imageSegment, imgray, COLOR_BGR2GRAY);
-    imshow("Display Image", imgray);
+    createTrackbar("Canny threshold", "Display Image", &edgeThresh, 400, onTrackbar);
+    onTrackbar(0, nullptr);
     waitKey(0);
-    std::vector< std::vector<Point> > contours;
-    findContours(imgray, contours, RETR_TREE, CHAIN_APPROX_NONE);
-    Mat mask = Mat::zeros(image.rows, image.cols, CV_8UC1);
-    drawContours(mask, contours, -1, Scalar(255), LINE_AA);
-    imshow("Display Image", mask);
-    waitKey(0);
-
     return 0;
 }
